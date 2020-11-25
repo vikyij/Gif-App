6be2121d85b4a94eb0c4148c9dd10b1ceabde9d0
vikyij <vikkyij54@gmail.com>
2020-11-25 17:36:41 +0100

    built the component for random gifs and random named gifs

--- src/components/NamedGif.js
+++ src/components/NamedGif.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const NamedGif =() => {
-    return(
-        <h1>Named Gif</h1>
+const API_KEY = process.env.REACT_APP_API_KEY
+
+const NamedGif = () => {
+  const [gif, setGif] = useState('')
+  const [name, setName] = useState('love')
+
+  const fetchGif = async (name) => {
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${name}`
+    const { data } = await axios.get(url)
+    
+    const imgSrc = data.data.images.downsized_large.url
+
+    setGif(imgSrc)
+}
+
+    useEffect(() => {
+        fetchGif(name)
+    }, [name])
+
+  const handleClick = () => {
+      fetchGif(name)
+  }
+
+    return (
+        <div className="container">
+        <h1>Random {name} Gif</h1>
+        <img width="500" src={gif} alt="a random gif" />
+        <input value={name} onChange={(e) => setName(e.target.value)} placeholder="enter a searchterm"/>
+        <button onClick={handleClick}>Click Me!</button>
+        </div>
     )
 }
 
--- src/components/RandomGif.js
+++ src/components/RandomGif.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-const RandomGif =() => {
-    return(
-        <h1>random Gif</h1>
+const API_KEY = process.env.REACT_APP_API_KEY
+
+const RandomGif = () => {
+  const [gif, setGif] = useState('')
+
+  const fetchGif = async () => {
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`
+    const { data } = await axios.get(url)
+    
+    const imgSrc = data.data.images.downsized_large.url
+
+    setGif(imgSrc)
+}
+
+    useEffect(() => {
+        fetchGif()
+    }, [])
+
+  const handleClick = () => {
+      fetchGif()
+  }
+
+    return (
+        <div className="container">
+        <h1>Random Gif</h1>
+        <img width="500" src={gif} alt="a random gif" />
+        <button onClick={handleClick}>Click Me!</button>
+        </div>
     )
 }
 
